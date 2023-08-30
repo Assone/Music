549ed9207d6af5cb7d03f0b2c5d0c8e03075e364
Assone <14324389+Assone@users.noreply.github.com>
2023-08-30 23:29:18 +0800

    refactor: remove unused imports and commented code
    - Remove `similarKeys` import from `@/services/query/keys`
    - Comment out `similarArtist` query and its console log in `AlbumDetailView.tsx`

--- src/views/AlbumDetailView.tsx
+++ src/views/AlbumDetailView.tsx
@@ -1,4 +1,4 @@
-import { artistKeys, similarKeys } from '@/services/query/keys';
+import { artistKeys } from '@/services/query/keys';
 import { AlbumDetailRoute } from '@/services/router/map';
 import { cx } from '@emotion/css';
 import { css } from '@emotion/react';
@@ -44,13 +44,11 @@ const AlbumDetailView: React.FC = () => {
     [artistAlbumsQuery.data?.pages],
   );
 
-  const similarArtist = useQuery({
-    ...similarKeys.artist(data?.artist.id || 0),
-    // enabled: !!data?.artist.id,
-    enabled: false,
-  });
-
-  console.log(similarArtist);
+  // const similarArtist = useQuery({
+  //   ...similarKeys.artist(data?.artist.id || 0),
+  //   // enabled: !!data?.artist.id,
+  //   enabled: false,
+  // });
 
   const publishTime = useMemo(
     () =>
