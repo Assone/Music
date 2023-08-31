3a2dc4f49eb8f7649cbbc58e9cb8e099bcd3b07b
Assone <14324389+Assone@users.noreply.github.com>
2023-08-31 14:29:24 +0800

    refactor: refactor Sentry initialization and integration
    - Remove `Sentry.Replay` initialization in `Sentry.init` function
    - Add new integration to `Sentry` using `Sentry.onLoad` function

--- src/services/sentry.ts
+++ src/services/sentry.ts
@@ -8,14 +8,18 @@ Sentry.init({
       // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
       tracePropagationTargets: ['localhost'],
     }),
-    new Sentry.Replay({
-      maskAllText: true,
-      blockAllMedia: true,
-    }),
   ],
   // Performance Monitoring
   tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
   // Session Replay
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+Sentry.onLoad(() => {
+  Sentry.getCurrentHub()
+    .getClient()
+    ?.addIntegration?.(
+      new Sentry.Replay({ maskAllText: true, blockAllMedia: true }),
+    );
 });
