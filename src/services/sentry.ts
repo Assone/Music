69c4cfffa282ab09e6ece1c645d01b9ae6b38942
Assone <14324389+Assone@users.noreply.github.com>
2023-08-31 10:10:12 +0800

    refactor: refactor Sentry initialization and tracing configuration
    - Remove `import.meta.env.VITE_APP_API_BASEURL` from `tracePropagationTargets` in `Sentry.init()`
    - Set `tracePropagationTargets` in `Sentry.BrowserTracing` to only `'localhost'`

--- src/services/sentry.ts
+++ src/services/sentry.ts
@@ -6,10 +6,7 @@ Sentry.init({
   integrations: [
     new Sentry.BrowserTracing({
       // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-      tracePropagationTargets: [
-        'localhost',
-        import.meta.env.VITE_APP_API_BASEURL,
-      ],
+      tracePropagationTargets: ['localhost'],
     }),
     new Sentry.Replay({
       maskAllText: true,
