b156af75af23204e7bcdb38a597b6cb9a7d517a4
Assone <14324389+Assone@users.noreply.github.com>
2023-08-31 10:31:41 +0800

    feat: add error test functionality and improve typography component
    - Added a new function `onTriggerErrorTest` to HomeView that throws an error when triggered
    - Implemented a clickable text component "Trigger Error Test" in HomeView
    - Added a title "Playlist" in HomeView
    - Imported `HTMLAttributes` from `react` in Text.tsx
    - Extended `TextProps` interface to include `HTMLAttributes<HTMLElement>` in Text.tsx
    - Spread `props` in Text component and added a `className` value of "text-black dark:text-white" to the `span` element in Text.tsx

--- src/components/Typography/Text.tsx
+++ src/components/Typography/Text.tsx
@@ -1,9 +1,15 @@
-import { PropsWithChildren } from 'react';
+import { HTMLAttributes, PropsWithChildren } from 'react';
 
-export interface TextProps {}
+export interface TextProps extends HTMLAttributes<HTMLElement> {}
 
-const Text: React.FC<PropsWithChildren<TextProps>> = ({ children }) => (
-  <span>{children}</span>
+const Text: React.FC<PropsWithChildren<TextProps>> = ({
+  children,
+  ...props
+}) => (
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  <span {...props} className="text-black dark:text-white">
+    {children}
+  </span>
 );
 
 export default Text;
