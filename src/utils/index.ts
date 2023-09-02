7acb1b7ceaec356cc6bfba84fdb9e621a7d27733
Assone <14324389+Assone@users.noreply.github.com>
2023-09-02 13:03:40 +0800

    feat: add `arraify` function to `utils` module
    - Add `arraify` function to `utils` module for converting items into an array.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,5 @@
 // eslint-disable-next-line import/prefer-default-export
 export const noop = () => {};
+
+export const arraify = <T>(target: T | T[]): T[] =>
+  Array.isArray(target) ? target : [target];
