fec027faf3b76d6315d8a593d19b779932a4aec1
Assone <14324389+Assone@users.noreply.github.com>
2023-09-02 15:10:49 +0800

    feat: refactor playlist detail view component
    - Add conditional rendering of playlist details based on `isFetched`
    - Adjust JSX nesting and add conditional rendering for child components
    - Style and layout the playlist details section
    - Add multiple instances of `PlayButton` with a 4-pixel gap
    - Pass `cover` and `artists` props to `TrackList` component

--- src/views/PlaylistDetailView.tsx
+++ src/views/PlaylistDetailView.tsx
@@ -4,23 +4,25 @@ import { useRouteContext } from '@tanstack/react-router';
 
 const PlaylistDetailView: React.FC = () => {
   const { queryOptions } = useRouteContext({ from: PlaylistDetailRoute.id });
-  const { data } = useQuery(queryOptions);
+  const { data, isFetched } = useQuery(queryOptions);
   const trackIds = useMemo(() => data?.trackIds || [], [data?.trackIds]);
 
   return (
     <div>
-      <div className="flex flex-col gap-2 p-4">
-        <div className="flex flex-col items-center justify-center gap-2">
-          <Cover src={data?.cover} />
-          <h2 className="text-center text-xl font-bold dark:text-gray-100">
-            {data?.name}
-          </h2>
+      {isFetched && (
+        <div className="flex flex-col gap-2 p-4">
+          <div className="flex flex-col items-center justify-center gap-2">
+            <Cover src={data?.cover} />
+            <h2 className="text-center text-xl font-bold dark:text-gray-100">
+              {data?.name}
+            </h2>
+          </div>
+          <div className="flex gap-4 p-2">
+            <PlayButton />
+            <PlayButton />
+          </div>
         </div>
-        <div className="flex gap-4 p-2">
-          <PlayButton />
-          <PlayButton />
-        </div>
-      </div>
+      )}
       <TrackList className="px-4" trackIds={trackIds} cover artists />
     </div>
   );
