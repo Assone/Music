5a0a9b41dd1ac51a9b281bcbebc404c7d5944c50
Assone <14324389+Assone@users.noreply.github.com>
2023-09-03 14:48:07 +0800

    feat: add interval refresh feature to useReloadPrompt hook
    - Added constant `intervalMS` with value `60 * 60 * 1000` to [src/hooks/useReloadPrompt.tsx]
    - Modified `useRegisterSW` hook in [src/hooks/useReloadPrompt.tsx]:
      - Added `onRegistered` callback to set an interval to call `r.update()` every `intervalMS` milliseconds
      - Added `onRegisterError` callback to log any registration errors

--- src/hooks/useReloadPrompt.tsx
+++ src/hooks/useReloadPrompt.tsx
@@ -1,12 +1,32 @@
 import { toast } from 'react-hot-toast';
 import { useRegisterSW } from 'virtual:pwa-register/react';
 
+const intervalMS = 60 * 60 * 1000;
+
 export default function useReloadPrompt() {
   const {
     needRefresh: [needRefresh],
     offlineReady: [offlineReady],
     updateServiceWorker,
-  } = useRegisterSW();
+  } = useRegisterSW({
+    onRegistered(r) {
+      // eslint-disable-next-line prefer-template
+      if (r) {
+        setInterval(() => {
+          r.update().catch((error) => {
+            console.error(
+              '%c[Error]: updateServiceWorker',
+              'color: red; ',
+              error,
+            );
+          });
+        }, intervalMS);
+      }
+    },
+    onRegisterError(error) {
+      console.log('SW registration error', error);
+    },
+  });
 
   const content = useCallback(
     () => (
