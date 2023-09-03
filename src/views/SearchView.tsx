f3416dced9c8a31c3e31e69c94b55548fb68d1b2
Assone <14324389+Assone@users.noreply.github.com>
2023-09-03 20:10:51 +0800

    feat: add search input

--- src/views/SearchView.tsx
+++ src/views/SearchView.tsx
@@ -1,5 +1,12 @@
 const SearchView: React.FC = () => (
-  <div className="dark:text-white">SearchView</div>
+  <div className="p-4 flex flex-col gap-2">
+    <Typography.Title>Search</Typography.Title>
+    <input
+      type="text"
+      className="w-full p-2 rounded dark:bg-gray-800"
+      placeholder="想听什么？"
+    />
+  </div>
 );
 
 export default SearchView;
