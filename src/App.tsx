389e5ffe3dfbc7d352b561dcba8648d99d533ae9
Assone <a3onny@gmail.com>
2024-01-07 16:53:53 +0800

    feat: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,5 @@
 function App() {
-  return (
-    <>
-      <div />
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
