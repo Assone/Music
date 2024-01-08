290c6fab404319c733eaba0fec4cdc2b47094629
Assone <a3onny@gmail.com>
2024-01-08 13:39:19 +0800

    refactor: router.tsx to remove unused imports and update createRouter function

--- src/router.tsx
+++ src/router.tsx
@@ -1,22 +1,12 @@
-import {
-  QueryClient,
-  QueryClientProvider,
-  dehydrate,
-  hydrate,
-} from "@tanstack/react-query";
+import { QueryClientProvider, dehydrate, hydrate } from "@tanstack/react-query";
 import { Router } from "@tanstack/react-router";
 import { ReactNode } from "react";
+import queryClient from "./services/query-client";
 import { routeTree } from "./services/routes";
 
 // eslint-disable-next-line import/prefer-default-export
-export const createRouter = () => {
-  // Make sure you create your loader client or similar data
-  // stores inside of your `createRouter` function. This ensures
-  // that your data stores are unique to each request and
-  // always present on both server and client.
-  const queryClient = new QueryClient();
-
-  return new Router({
+export const createRouter = () =>
+  new Router({
     routeTree,
     defaultPreload: "intent",
     // Optionally provide your loaderClient to the router context for
@@ -40,7 +30,6 @@ export const createRouter = () => {
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     ),
   });
-};
 
 declare module "@tanstack/react-router" {
   interface Register {
