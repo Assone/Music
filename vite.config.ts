b4c768211030c1f4166b247c3292ca680b8bdf3c
Assone <a3onny@gmail.com>
2024-01-15 22:01:53 +0800

    feat: add auto-imports for RxJS library

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
         enabled: true,
       },
       dts: true,
-      imports: ['react'],
+      imports: ['react', 'rxjs'],
       resolvers: [
         IconsResolver({
           prefix: 'Icons',
