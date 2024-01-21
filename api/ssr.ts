d9a4c32df5954423dd898f02e556d520b787693f
Assone <14324389+Assone@users.noreply.github.com>
2024-01-21 22:35:11 +0800

    feat: add Vercel configuration and SSR endpoint

---
+++ api/ssr.ts
@@ -0,0 +1,33 @@
+/* eslint-disable no-console */
+import fs from 'node:fs';
+import path from 'path';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+// eslint-disable-next-line consistent-return
+export default async function handler(
+  request: VercelRequest,
+  response: VercelResponse,
+) {
+  const { url } = request;
+  console.log('request', url);
+
+  if (!url) return response.status(400);
+
+  const template = fs.readFileSync(
+    path.resolve(process.cwd(), 'dist/client/index.html'),
+    'utf-8',
+  );
+  const { render } = (await import(
+    path.resolve(process.cwd(), './dist/server/entry-server.js')
+  )) as { render: (...args: unknown[]) => Promise<void> };
+
+  const newRequest = Object.assign(request, { protocol: 'https' });
+  console.log('rendering', url);
+
+  await render(url, {
+    request: newRequest,
+    response,
+    template,
+    isProduction: true,
+  });
+}
